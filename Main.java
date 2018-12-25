829546a0c3678f58cd5f27e90f72b9867ce7f711
lirui <lirui@cetiti.com>
2018-12-25 14:07:27 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package me.moon;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author LRui
+ * @date 2018-12-14 11:36
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new ArrayList<>();
+        new LinkedList<>();
+        new HashMap<>();
+        new LinkedHashMap<>();
+        new TreeMap<>();
+        new CopyOnWriteArrayList<>();
+        System.out.println(2 >> 1);
+    }
+}
